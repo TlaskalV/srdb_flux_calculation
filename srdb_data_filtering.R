9c15ca4aa9a019f6e5c7aac07fbb08f850bfb3c2
Vojczech <tlaskal@biomed.cas.cz>
2020-02-08 16:47:06 +0100

    initial commit script calculates mean of annual CO2 flux throughout the forests in individual biomes

---
+++ srdb_data_filtering.R
@@ -0,0 +1,23 @@
+library(readr)
+library(RCurl)
+library(tidyverse)
+
+# download 20191022, 
+# commit 8df6b9e394bb0f8c44a62e535ec9c09836756b46
+
+srdb_table <- read.csv(text = getURL("https://raw.githubusercontent.com/bpbond/srdb/master/srdb-data.csv"))
+
+srdb_table_temperate_forests <- tbl_df(srdb_table) %>% 
+  filter(Quality_flag != "Q10") %>% # filter irrelevant quality flags
+  filter(Quality_flag != "Q11") %>%
+  filter(Quality_flag != "Q12") %>%
+  filter(Quality_flag != "Q13") %>%
+  filter(Quality_flag != "Q14") %>% 
+  filter(Manipulation == "None") %>% # only unmanipulated sites
+  filter(Ecosystem_type == "Forest") %>%  
+  drop_na(Rs_annual) %>% # omit NA in annual Rs
+  group_by(Biome) %>%
+  summarise(median_g_C_m_yr = median(Rs_annual), mean_g_C_m_yr = mean(Rs_annual), sd = sd(Rs_annual), n = n()) %>% 
+  mutate(median_Mg_C_ha_yr = median_g_C_m_yr/100, mean_Mg_C_ha_yr = mean_g_C_m_yr/100) %>% 
+  print
+  
